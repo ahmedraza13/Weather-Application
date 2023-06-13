d1f232083c1067b39372c1f0cda4d12df8c1b516
ahmedraza13 <ahmedfaisalakhawala@gmail.com>
2023-06-13 17:18:41 +0500

    complete

--- app.mjs
+++ app.mjs
@@ -10,18 +10,23 @@ window.getWeather = function () {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
     .then(function (response) {
       // handle success
-      
+      console.log(response.data)
       let cityName = response.data.name;
       let currentTime = moment().format('MMMM Do hh:mm A');
       let icon = response.data.weather[0].icon;
       let weatherDescription = response.data.weather[0].description;
       let currentTemperature = `${Math.round(response.data.main.temp)}°`
+      
+      
+
+
 
       document.querySelector(".city").innerHTML = cityName;
       document.querySelector(".time").innerHTML = currentTime;
       document.querySelector("#icon1").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
       document.querySelector("#condition").innerHTML = weatherDescription
-      document.querySelector(".ctemp").innerHTML = currentTemperature;
+      document.querySelector(".curtemp").innerHTML = currentTemperature;
+      
       
      
     })
